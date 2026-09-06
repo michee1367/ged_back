1704513a24323fbaad16ff3a7b35ef920463eda4
Michée NKUSU <nkusumichee1367@gmail.com>
2026-09-06 07:04:51 +0200

    add

--- src/main/java/com/mich/ged/config/OpenApiConfig.java
+++ src/main/java/com/mich/ged/config/OpenApiConfig.java
@@ -1,5 +1,7 @@
 package com.mich.ged.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,6 +9,7 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
@@ -17,6 +20,12 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("https://apiged.mink67.com/api/v1").description("Serveur HTTPS Production prefix"),
+                        new Server().url("https://apiged.mink67.com").description("Serveur HTTPS Production"),
+                        new Server().url("http://localhost:8182").description("Serveur Local Dev"),
+                        new Server().url("http://localhost:8182/api/v1").description("Serveur Local Dev prefix")
+                ))
                 .info(new Info()
                         .title("GED API")
                         .version("1.0"))
